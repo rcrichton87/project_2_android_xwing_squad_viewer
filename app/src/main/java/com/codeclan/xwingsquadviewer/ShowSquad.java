8322604de222011b08de47234f2121f1fd108e86
Ross Crichton <rcrichton87@googlemail.com>
2017-03-17 17:04:47 +0000

    add win and and add loss buttons work on a squad page

--- app/src/main/java/com/codeclan/xwingsquadviewer/ShowSquad.java
+++ app/src/main/java/com/codeclan/xwingsquadviewer/ShowSquad.java
@@ -1,19 +1,24 @@
 package com.codeclan.xwingsquadviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ShowSquad extends AppCompatActivity {
+
+    Squad squad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_squad);
 
-        Squad squad = (Squad) getIntent().getSerializableExtra("squad");
+        squad = (Squad) getIntent().getSerializableExtra("squad");
 
         Log.d("Squad", squad.getName());
 
@@ -32,5 +37,21 @@ public class ShowSquad extends AppCompatActivity {
         TextView squadDetails = (TextView) findViewById(R.id.squad_details);
         squadDetails.setText(squad.getDetails());
 
+    }
+
+    public void addWinClicked(View view){
+        Intent intent = getIntent();
+        squad.addWin();
+        intent.putExtra("squad", squad);
+        finish();
+        startActivity(intent);
+    }
+
+    public void addLossClicked(View view){
+        Intent intent = getIntent();
+        squad.addLoss();
+        intent.putExtra("squad", squad);
+        finish();
+        startActivity(intent);
     }
 }
