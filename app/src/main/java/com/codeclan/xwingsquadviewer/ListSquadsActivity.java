6067eb3225467bad8bd6c30be3114c2c03d27ee3
Ross Crichton <rcrichton87@googlemail.com>
2017-03-18 18:48:51 +0000

    added a menu to the squads list

--- app/src/main/java/com/codeclan/xwingsquadviewer/ListSquadsActivity.java
+++ app/src/main/java/com/codeclan/xwingsquadviewer/ListSquadsActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -45,4 +48,22 @@ public class ListSquadsActivity extends AppCompatActivity {
         intent.putExtra("squad", squad);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.squads_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.action_new_squad){
+            Intent intent = new Intent(this, NewSquad.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/codeclan/xwingsquadviewer/NewSquad.java
@@ -0,0 +1,6 @@
+package com.codeclan.xwingsquadviewer;
+
+
+public class NewSquad {
+
+}
