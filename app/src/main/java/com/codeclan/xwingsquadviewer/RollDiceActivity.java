6d7505fb8e187f08b126a62b8eb3d2edab9cba7c
Ross Crichton <rcrichton87@googlemail.com>
2017-03-21 14:27:06 +0000

    added a sound effect to the dice roller

--- app/src/main/java/com/codeclan/xwingsquadviewer/RollDiceActivity.java
+++ app/src/main/java/com/codeclan/xwingsquadviewer/RollDiceActivity.java
@@ -1,6 +1,7 @@
 package com.codeclan.xwingsquadviewer;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -84,6 +85,9 @@ public class RollDiceActivity extends AppCompatActivity {
     }
 
     public void rollButtonClicked(View button){
+
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.dice);
+        mp.start();
 
         int diceTypeButton = diceType.getCheckedRadioButtonId();
         if (diceTypeButton == attackDiceButton.getId() ){
