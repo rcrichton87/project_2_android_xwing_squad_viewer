e8478630eb422e6423a999b4a4770fd061c4f788
Ross Crichton <rcrichton87@googlemail.com>
2017-03-21 11:12:47 +0000

    ListSquadsAdapter cleaned up

--- app/src/main/java/com/codeclan/xwingsquadviewer/ListSquadsAdapter.java
+++ app/src/main/java/com/codeclan/xwingsquadviewer/ListSquadsAdapter.java
@@ -1,18 +1,13 @@
 package com.codeclan.xwingsquadviewer;
 
-
 import android.content.Context;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 public class ListSquadsAdapter extends ArrayAdapter<Squad> {
@@ -25,7 +20,6 @@ public class ListSquadsAdapter extends ArrayAdapter<Squad> {
     public View getView(int position, View convertView, ViewGroup parent){
 
         View listItemView = convertView;
-
         if (listItemView == null){
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.squads_item, parent, false);
         }
@@ -33,7 +27,6 @@ public class ListSquadsAdapter extends ArrayAdapter<Squad> {
         Squad currentSquad = getItem(position);
 
         listItemView.setTag(currentSquad);
-        Log.d("setTag Squad", currentSquad.toString());
 
         ImageView factionSymbol = (ImageView) listItemView.findViewById(R.id.faction_symbol);
         factionSymbol.setImageResource(currentSquad.getFactionSymbol());
@@ -55,16 +48,15 @@ public class ListSquadsAdapter extends ArrayAdapter<Squad> {
         squadWinLossRatio.setText(currentSquad.getWinLossRatio().toString());
 
         if (currentSquad.getWinLossRatio() < 0 ) {
-            squadWinLossRatio.setTextColor(Color.parseColor("#df0000"));
+            squadWinLossRatio.setTextColor(Color.parseColor("#df0000")); //set to red if negative
         }
         if (currentSquad.getWinLossRatio() > 0 ) {
-            squadWinLossRatio.setText("+" + currentSquad.getWinLossRatio().toString());
-            squadWinLossRatio.setTextColor(Color.parseColor("#13df00"));
+            squadWinLossRatio.setText("+" + currentSquad.getWinLossRatio().toString()); // put a + in front of the number
+            squadWinLossRatio.setTextColor(Color.parseColor("#13df00")); //set to green if positive
         }
 
         return listItemView;
-
-
+        
     }
 
 }
