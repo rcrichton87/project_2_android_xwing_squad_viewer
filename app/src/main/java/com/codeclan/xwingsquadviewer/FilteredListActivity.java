fed1161e575c2d5a2a0d33d38650311025ef62db
Ross Crichton <rcrichton87@googlemail.com>
2017-03-21 10:57:39 +0000

    FilteredListActivity cleaned up

--- app/src/main/java/com/codeclan/xwingsquadviewer/FilteredListActivity.java
+++ app/src/main/java/com/codeclan/xwingsquadviewer/FilteredListActivity.java
@@ -1,12 +1,8 @@
 package com.codeclan.xwingsquadviewer;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
